c792c62c4a14b4f75cbb73153fa24d094c9beaf8
Jason Gritman <jasongritman@gmail.com>
2019-01-15 17:34:51 -0700

    Initial Commit

---
+++ WebViewDataDetectorTest/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WebViewDataDetectorTest
+//
+//  Created by Jason Gritman on 1/15/19.
+//  Copyright © 2019 Jason Gritman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
